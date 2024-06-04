8b92f780d6b0f2f2f886e62c8956fce40bc4bf76
zhco00 <mkni0216@gmail.com>
2024-06-04 14:43:55 +0900

    api주소 추가

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: '???',
+  baseURL: 'http://218.232.137.30:20080/api/',
   timeout: 1000,
   headers: { access_token: '???' },
 });
