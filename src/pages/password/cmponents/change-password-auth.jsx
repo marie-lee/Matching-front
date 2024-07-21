59d5ddcaec60c8b52662d1713f0e5700fe7ad0ce
47mango <do_hh@naver.com>
2024-07-21 12:42:30 +0900

    기능구현

--- src/pages/password/cmponents/change-password-auth.jsx
+++ src/pages/password/cmponents/change-password-auth.jsx
@@ -13,55 +13,76 @@ import {
     DialogContentText,
     DialogTitle
   } from '@mui/material';
-  import React, { useState, useEffect } from 'react';
-  import { RhfFormProvider, RhfTextField } from '@/components/hook-form/index';
-  import { PATHS } from '@/routes/paths';
-  import { Link, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { PATHS } from '@/routes/paths';
+import { Link, useNavigate } from 'react-router-dom';
 import { Email } from '@mui/icons-material';
+import {postEmailCertification,postEmailConfirmation} from '@/services/member';
 
 
 
 const AuthPassword = () => {
-    const theme = useTheme();
-
-    const [openDialog, setOpenDialog] = useState(false);
-    const [dialogContent, setDialogContent] = useState({ title: '', message: '', buttonText: '' });
-
-    const [emaildialog, setemaildialog] = useState({ title: '', message: '', buttonText: '' });
+    
+    const [name, setName] = useState('');
+    const [USER_EMAIL, setEmail] = useState('');
+    const [authCode, setAuthCode] = useState('');
 
 
-    const EmainCheck = () =>{
-        setemaildialog({
-            title:'일치하는 회원 정보가 없습니다',
-            buttonText : '확인'
-        })
-    }
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [dialogMessage, setDialogMessage] = useState('');
+
+    const [redirectPath, setRedirectPath] = useState(null);
+    const navigate = useNavigate();
 
 
-    const handleChangePassword = () => {
-        if (password === confirmPassword) {
-          setDialogContent({
-            title: '인증번호가 틀렸습니다',
-            message: '',
-            buttonText: '로그인하러 가기'
-          });
+    const isNameFilled = name.trim() !== '';
+    const isEmailFilled = USER_EMAIL.trim() !== '';
+    const isAuthCodeFilled = authCode.trim() !== '';
+
+    const [isEmailSent, setIsEmailSent] = useState(false);
+
+
+  //인증번호 전송
+  const handleEmailCertification = async () => {
+    try{    
+        const response = postEmailCertification(USER_EMAIL);
+        if (response.status === 200) {
+            setDialogMessage('해당 이메일로 인증번호를 보냈습니다.');
         } else {
-          setDialogContent({
-            title: '현재 비밀번호와 일치합니다.',
-            message: '다른 비밀번호를 입력해 주세요',
-            buttonText: '확인'
-          });
+            setDialogMessage('일치하는 회원 정보가 없습니다.');
         }
-        setOpenDialog(true);
-      };
-      
-      const handleCloseDialog = () => {
-        setOpenDialog(false);
-        if (password === confirmPassword) {
-          navigate(PATHS.auth.signIn); // 로그인 페이지로 이동
-        }
-      };
 
+    } catch (error) {
+        setDialogMessage('오류가 발생했습니다. 나중에 다시 시도해 주세요.');
+    } finally {
+        setDialogOpen(true);
+    }
+  };
+
+  const handleCloseDialog = () => {
+    setDialogOpen(false);
+};
+
+// 인증번호 확인
+  const handelEmailConfirmation = async () => {
+    try{
+        const response = postEmailConfirmation(USER_EMAIL,authCode);
+
+        if (response.status === 200) {
+            setDialogMessage('인증 완료되었습니다.');
+            setRedirectPath(PATHS.somePath); //이동경로 설정
+        } else {
+            setDialogMessage('인증번호가 틀렸습니다.');
+        }
+    }catch {
+        setDialogMessage('오류가 발생했습니다. 나중에 다시 시도해 주세요.');
+    } finally {
+        setDialogOpen(true);
+    }
+  }
+
+
+    
     return(
         <Box sx={{ maxWidth: 800, margin: '0 auto', mt: 10}}>
             <Box
@@ -94,70 +115,67 @@ const AuthPassword = () => {
                         <TextField
                             placeholder='이름을 입력해주세요'
                             sx={{marginBottom:2}}
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
                         />
                         <Stack direction="row" spacing={2}>
                             <TextField 
                                 placeholder='이메일을 입력해주세요'
                                 sx={{width:0.8}}
+                                value={USER_EMAIL}
+                                onChange={(e) => setEmail(e.target.value)}
                             />
                             <Button
-                                onClick={EmainCheck}
-                                sx={{width:0.2}}
+                                sx={{ width: 0.2, backgroundColor: isEmailFilled ? '#000000' : '#0000001F' }}
+                                disabled={!isEmailFilled}
+                                onClick={handleEmailCertification}
                             >인증번호 요청</Button>
                         </Stack>
-                        <Stack direction="row" spacing={2} sx={{marginBottom:2,marginTop:2}}>
                             <TextField 
                                 placeholder='인증번호를 입력해주세요'
-                                sx={{width:0.8}}
+                                sx={{marginBottom:2,marginTop:2}}
+                                value={authCode}
+                                onChange={(e) => setAuthCode(e.target.value)}
                             />
-                            <Button sx={{width:0.2}}>
-                                확인
-                            </Button>
-                        </Stack>
+                        
                     </Stack>
                 <Stack>
-                    <Button sx={{marginBottom:2}} onClick={handleChangePassword}>
+                    <Button 
+                        sx={{ marginBottom: 2, backgroundColor: isAuthCodeFilled ? '#000000' : '#0000001F' }}
+                        disabled={!isAuthCodeFilled}
+                        onClick={handelEmailConfirmation}
+                    >
                         다음
                     </Button>
                     <Box sx={{display: 'flex', justifyContent: 'center',marginTop:2}}>
                         <Link
-                            href="/auth/sign-in"
-                            underline="hover"
+                            href="#"
                             variant={'xs'}
                         >
                         로그인하러 가기
                         </Link>
                     </Box>
                 </Stack>
-
-                <Dialog>
-                    <DialogContent sx={{textAlign:'center'}}>
-                        <DialogTitle >{emaildialog.title}</DialogTitle>
-                    </DialogContent>
-                </Dialog>
-
-
-                <Dialog open={openDialog} onClose={handleCloseDialog}
-                    sx={{
-                    padding: 5,
-                    borderRadius: 2,
-                    boxShadow: '0px 6px 20px rgba(0, 0, 0, 0.1)',
-                    }}
-                >
-                    <DialogContent sx={{ textAlign: 'center' }}>
-                    <DialogTitle variant={'xl'}>{dialogContent.title}<br />{dialogContent.message}</DialogTitle>
-                    </DialogContent>
-                    <DialogActions>
-                    
-                    <Button onClick={handleCloseDialog} color="primary"
-                        sx={{width:1}}
-                    >
-                        {dialogContent.buttonText}
-                    </Button>
-                    
-                    </DialogActions>
-                </Dialog>
             </Box>
+
+            <Dialog
+                open={dialogOpen}
+                onClose={handleCloseDialog}
+                fullWidth
+                maxWidth="sm"
+            >
+                <DialogTitle sx={{ textAlign: 'center' }}>{dialogMessage}</DialogTitle>
+                <DialogActions>
+                    <Button 
+                        onClick={handleCloseDialog} 
+                        color="primary"
+                        sx={{ width: '100%', textAlign: 'center' }}
+                    >
+                        확인
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
         </Box>    
     );
     
--- src/services/member.js
+++ src/services/member.js
@@ -7,3 +7,25 @@ export const URL_POST_MEMBER_LOGIN = '/member/login';
 export const postMemberLogin = (payload) => {
   return instance.post(URL_POST_MEMBER_LOGIN, payload);
 };
+
+//이메일 인증 요청
+export const URL_POST_EMAIL_CERTIFICATION ='/member/registeration/certification';
+export const postEmailCertification = (USER_EMAIL) => {
+  console.log(USER_EMAIL);
+  return instance.post(URL_POST_EMAIL_CERTIFICATION,
+  {
+    USER_EMAIL:USER_EMAIL
+  });
+}
+
+//이메일 인증 확인
+export const URL_POST_EMAIL_CONFIRMATION = '/member/registeration/confirmation';
+export const postEmailConfirmation = (USER_EMAIL,verificationCode) => {
+  console.log(USER_EMAIL,verificationCode)
+  return instance.post(URL_POST_EMAIL_CONFIRMATION,
+    {
+      verificationCode : verificationCode,
+      USER_EMAIL : USER_EMAIL
+    }
+  );
+}
