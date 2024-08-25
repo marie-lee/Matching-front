fd69aacdfe44ea7cc74238ca5eea22bb8180c2ad
LimDong <50188317+Limdongdang@users.noreply.github.com>
2024-08-25 23:17:22 +0900

    팀원 리뷰 조회 추가

--- src/pages/review/components/member-review.jsx
+++ src/pages/review/components/member-review.jsx
@@ -3,11 +3,22 @@ import { Grid, Stack, Typography } from '@mui/material';
 const MemberReview = ({ review }) => {
   const reviews = [
     {
-      ANSWER: '프로젝트에서 아쉬운 부분이 많았습니다',
+      RATE_1: 5,
+      RATE_2: 4,
+      RATE_3: 3,
+      RATE_4: 2,
+      RATE_5: 1,
+      RATE_TEXT:
+        '열정적으로 프로젝트에 참가하여 다양한 문제를 잘 해결하는 모습이 보기 좋았습니다.',
     },
     {
-      ANSWER:
-        '열정적으로 프로젝트에 임하며 이슈발생시, 적극적으로 해결해나가는 모습이 인상깊었습니다.',
+      RATE_1: 3,
+      RATE_2: 4,
+      RATE_3: 5,
+      RATE_4: 2,
+      RATE_5: 1,
+      RATE_TEXT:
+        '프로젝트에서 맡은 역할을 잘 수행하였으나, 커뮤니케이션 부분에서 조금 더 신경 써주었으면 좋겠습니다.',
     },
   ];
   return (
@@ -16,19 +27,35 @@ const MemberReview = ({ review }) => {
         <Typography variant={'lg'} fontWeight={'fontWeightSemiBold'}>
           팀원 리뷰
         </Typography>
-        {reviews.map((review, index) => (
+        {review.length === 0 ?
           <Stack
-            key={index}
             spacing={2}
             p={3}
             bgcolor={'background.default'}
             borderRadius={1}
-            border={1}
-            borderColor={'divider'}
           >
-            <Typography variant={'body1'}>{review.ANSWER}</Typography>
+            <Typography
+              variant={'md'}
+              fontWeight={'fontWeight'}
+              color={'text.secondary'}
+            >
+              아직 팀원리뷰가 없습니다.
+            </Typography>
           </Stack>
-        ))}
+        : reviews.map((review, index) => (
+            <Stack
+              key={index}
+              spacing={2}
+              p={3}
+              bgcolor={'background.default'}
+              borderRadius={1}
+              border={1}
+              borderColor={'divider'}
+            >
+              <Typography variant={'body1'}>{review.RATE_TEXT}</Typography>
+            </Stack>
+          ))
+        }
       </Stack>
     </Grid>
   );
