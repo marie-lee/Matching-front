73cfa8202458b1a9b872a657c45001d6a040b0ce
hyewon0924 <67727434+hyewon0924@users.noreply.github.com>
2024-09-08 02:15:59 +0900

    path 변경 시, 스크롤 최상단으로 이동되도록 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import '@/global.css';
 
 import Router from '@/routes';
+import { useScrollToTop } from '@/hooks/use-scroll-to-top';
 
 // ----------------------------------------------------------------------
 
 function App() {
+  useScrollToTop();
+
   return <Router />;
 }
 export default App;
