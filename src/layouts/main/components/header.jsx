d68f2abb3f037bea0cda2f7b145a908f15de01ba
marie.lee <mariesejinlee@gmail.com>
2024-09-09 10:54:01 +0900

    준비중인 서비스 처리, 로고 변경

--- src/layouts/main/components/header.jsx
+++ src/layouts/main/components/header.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import {
   AppBar,
   Box,
@@ -6,61 +7,69 @@ import {
   Link,
   Stack,
   Toolbar,
-  Typography,
   useTheme,
 } from '@mui/material';
 
 import { ProfilePopover, MenuPopover } from '@/layouts/components';
 import { HEADER } from '@/layouts/config-layout';
 import { Icon } from '@iconify/react';
-
-// ----------------------------------------------------------------------
+import PreparingService from '@/components/custom-popover/preparing-service';
 
 const Header = () => {
   const theme = useTheme();
 
-  // ----------------------------------------------------------------------
+  const [isPreparingOpen, setIsPreparingOpen] = useState(false);
+
+  const handleOpenPreparingService = () => {
+    setIsPreparingOpen(true);
+  };
+
+  const handleClosePreparingService = () => {
+    setIsPreparingOpen(false);
+  };
 
   return (
-    <AppBar
-      sx={{
-        backgroundColor: 'transparent',
-        color: theme.palette.text.primary,
-      }}
-    >
-      <Toolbar
+    <>
+      <AppBar
         sx={{
-          px: `0 !important`,
-          height: HEADER.H_DESKTOP,
-          backgroundColor: theme.palette.background.default,
-          borderBottom: `1px solid ${theme.palette.grey[300]}`,
+          backgroundColor: 'transparent',
+          color: theme.palette.text.primary,
         }}
       >
-        <Container sx={{ display: 'flex', alignItems: 'center' }}>
-          <Link
-            href={'/'}
-            variant={'lg'}
-            fontFamily={'Esamanru'}
-            underline={'none'}
-            fontWeight={'fontWeightMedium'}
-          >
-            Project Matching
-          </Link>
+        <Toolbar
+          sx={{
+            px: `0 !important`,
+            height: HEADER.H_DESKTOP,
+            backgroundColor: theme.palette.background.default,
+            borderBottom: `1px solid ${theme.palette.grey[300]}`,
+          }}
+        >
+          <Container sx={{ display: 'flex', alignItems: 'center' }}>
+            <Link
+              href={'/'}
+              variant={'lg'}
+              fontFamily={'Esamanru'}
+              underline={'none'}
+              fontWeight={'fontWeightMedium'}
+            >
+              Hoit!
+            </Link>
 
-          {/*<NavDesktop data={configNavigation} />*/}
+            <Box sx={{ flexGrow: 1 }} />
 
-          <Box sx={{ flexGrow: 1 }} />
+            <Stack direction={'row'} spacing={1}>
+              <IconButton onClick={handleOpenPreparingService}>
+                <Icon icon={'iconoir:bell'} fontSize={28} />
+              </IconButton>
+              <MenuPopover />
+              <ProfilePopover />
+            </Stack>
+          </Container>
+        </Toolbar>
+      </AppBar>
 
-          <Stack direction={'row'} spacing={1}>
-            <IconButton>
-              <Icon icon={'iconoir:bell'} fontSize={28} />
-            </IconButton>
-            <MenuPopover />
-            <ProfilePopover />
-          </Stack>
-        </Container>
-      </Toolbar>
-    </AppBar>
+      <PreparingService open={isPreparingOpen} onClose={handleClosePreparingService} />
+    </>
   );
 };
 
