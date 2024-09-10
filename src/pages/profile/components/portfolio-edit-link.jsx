4a77b65e3e0ec5744a56309a4b0a2b245cc63f92
LimDong <50188317+Limdongdang@users.noreply.github.com>
2024-09-10 22:28:27 +0900

    포트폴리오 url 추가 안 되는 오류 해결

---
+++ src/pages/profile/components/portfolio-edit-link.jsx
@@ -0,0 +1,81 @@
+import { RhfTextField } from '@/components/hook-form';
+import { Box, Button, IconButton, Stack } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import { useEffect, useState } from 'react';
+import { useTheme } from '@emotion/react';
+import { useFieldArray, useFormContext } from 'react-hook-form';
+
+export const PortfolioLink = ({ index }) => {
+  const theme = useTheme();
+  const { control, register } = useFormContext();
+  const { fields, append } = useFieldArray({
+    control,
+    name: `portfolioInfo[${index}].url`,
+  });
+
+  const handleAppendLink = () => {
+    append({ URL: '', URL_INTRO: '' });
+  };
+
+  useEffect(() => {
+    fields.forEach((field, fieldIndex) => {
+      register(`portfolioInfo[${index}].url[${fieldIndex}].URL`);
+      register(`portfolioInfo[${index}].url[${fieldIndex}].URL_INTRO`);
+    });
+  }, [fields, index, register]);
+
+  return (
+    <Box
+      sx={{
+        p: 2,
+        border: '1px solid',
+        borderColor: 'divider',
+        borderRadius: '4px',
+      }}
+    >
+      <Stack spacing={1}>
+        {fields.map((field, linkIndex) => (
+          <Stack
+            key={field.id}
+            direction={'row'}
+            spacing={1}
+            alignItems={'center'}
+          >
+            <Box>
+              <RhfTextField
+                name={`portfolioInfo[${index}].url[${linkIndex}].URL`}
+                label={'URL'}
+                size={'medium'}
+                variant={'outlined'}
+              />
+            </Box>
+            <Box sx={{ flexGrow: 1 }}>
+              <RhfTextField
+                name={`portfolioInfo[${index}].url[${linkIndex}].URL_INTRO`}
+                label={'설명'}
+                size={'medium'}
+                variant={'outlined'}
+              />
+            </Box>
+            <Box>
+              <IconButton>
+                <AddIcon />
+              </IconButton>
+            </Box>
+          </Stack>
+        ))}
+      </Stack>
+      <Button
+        color="primary"
+        variant={'outlined'}
+        fullWidth
+        startIcon={
+          <AddIcon sx={{ color: theme.palette.text.primary }}></AddIcon>
+        }
+        onClick={handleAppendLink}
+      >
+        추가하기
+      </Button>
+    </Box>
+  );
+};
