bba45b979c413cbf6728edbfba177cee3cedbbed
marie.lee <mariesejinlee@gmail.com>
2024-10-16 09:49:58 +0900

    홈에서만 가이드 팝업 보이도록 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,14 @@
-import '@/global.css';
-import { useEffect, useState } from 'react';
-import Router from '@/routes';
 import { useScrollToTop } from '@/hooks/use-scroll-to-top';
-import GuidePopup from '@/pages/guide/guide-popup-page';
+import Router from '@/routes';
 
 // ----------------------------------------------------------------------
 
 function App() {
   useScrollToTop();
 
-  const [openGuidePopup, setOpenGuidePopup] = useState(false);
-
-  useEffect(() => {
-    const hideGuidePopup = localStorage.getItem('hideGuidePopup');
-    if (!hideGuidePopup) {
-      setOpenGuidePopup(true);
-    }
-  }, []);
-
-  const handleCloseGuidePopup = () => {
-    setOpenGuidePopup(false);
-  };
-
   return (
     <>
       <Router />
-      <GuidePopup open={openGuidePopup} onClose={handleCloseGuidePopup} initialTab={0} showCheckbox={true} />
     </>
   );
 }
